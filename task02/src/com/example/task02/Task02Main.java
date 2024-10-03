f0e8954a94f47a08eb38d2ff2990b02ae252a7ab
leclud <negativchikxxl@gmail.com>
2024-10-03 23:42:05 +0500

    task 2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,16 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+        TimeSpan time1 = new TimeSpan(1, 150, 70); // 1 час, 150 минут, 70 секунд
+        System.out.println("TimeSpan 1: " + time1); // Должно быть 03:01:10
 
+        TimeSpan time2 = new TimeSpan(0, 30, 30); // 30 минут, 30 секунд
+        System.out.println("TimeSpan 2: " + time2); // 00:30:30
+
+        time1.add(time2);
+        System.out.println("После добавления: " + time1); // Должно быть 03:31:40
+
+        time1.subtract(new TimeSpan(1, 0, 0)); // Вычитаем 1 час
+        System.out.println("После вычитания: " + time1); // Должно быть 02:31:40
     }
 }
